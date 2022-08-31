bf5e88826542048ec863939eb5e0cd80dd5da864
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-31 18:41:56 +0530

    it is udemy exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello world!");
+=======
+        Wall wall = new Wall(5.0,4.0);
+         System.out.println("area= " + wall.getArea());
+
+         wall.setHeight(-1.5);
+         System.out.println("width= " + wall.getWidth());
+         System.out.println("height= " + wall.getHeight());
+         System.out.println("area= " + wall.getArea());
+>>>>>>> 8a3efce (it is udemy exercise.)
     }
 }
--- src/Wall.java
+++ src/Wall.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Wall {
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(0.00, 0.00);
+    }
+
+
+    public Wall(double width, double height) {
+        if (height < 0 && width > 0) {
+            this.width = width;
+            this.height = 0;
+        } else if (width < 0 && height > 0) {
+            this.width = 0;
+            this.height = height;
+        } else if (width < 0 && height < 0) {
+            this.width = 0;
+            this.height = 0;
+        } else {
+            this.width = width;
+            this.height = height;
+        }
+
+    }
+
+    public double getArea() {
+        return height * width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width > 0)
+            this.width = width;
+        else
+            this.width = 0;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height > 0)
+            this.height = height;
+        else
+            this.height = 0;
+    }
 }
+
